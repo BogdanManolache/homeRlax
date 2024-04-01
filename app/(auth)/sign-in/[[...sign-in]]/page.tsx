81ed270460babbb5dfdeee1983e30c2926827c08
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2024-04-01 21:36:11 +0300

    updated sign-in and sign-up pages styling

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -6,7 +6,14 @@ export default function SignInPage() {
       className="mx-auto flex min-h-screen max-w-2xl items-center justify-center md:max-w-7xl"
       id="signIn"
     >
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            formButtonPrimary: 'bg-primary hover:bg-primary/80 duration-300',
+            footerActionLink: 'text-primary/80 hover:text-primary',
+          },
+        }}
+      />
     </section>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,14 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <section className="mx-auto flex min-h-screen max-w-2xl items-center justify-center md:max-w-7xl">
-      <SignUp />
+      <SignUp
+        appearance={{
+          elements: {
+            formButtonPrimary: 'bg-primary hover:bg-primary/80 duration-300',
+            footerActionLink: 'text-primary/80 hover:text-primary',
+          },
+        }}
+      />
     </section>
   );
 }
