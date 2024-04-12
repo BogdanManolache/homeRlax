808091d567846fe829e571b3206cbef8b22e539a
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2024-04-12 13:12:38 +0300

    installed framer motion and modified navbar styling

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
 
 const links = [
   { name: 'Home', href: '/' },
@@ -11,18 +13,39 @@ const links = [
 
 export default function Navbar() {
   const pathname = usePathname();
+  const MotionLink = motion(Link);
+
+  const mapRange = (
+    inputLower: number,
+    inputUpper: number,
+    outputLower: number,
+    outputUpper: number,
+  ) => {
+    const INPUT_RANGE = inputUpper - inputLower;
+    const OUTPUT_RANGE = outputUpper - outputLower;
+
+    return (value: number) => {
+      outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0);
+    };
+  };
 
   return (
-    <nav className="hidden md:flex md:gap-1 lg:gap-2">
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          href={link.href}
-          className={`px-4 py-2 font-medium lg:text-lg ${pathname === link.href ? ' text-primary' : 'text-foreground duration-200 hover:text-primary'}`}
-        >
-          {link.name}
-        </Link>
-      ))}
+    <nav className="hidden md:block">
+      <ul className="flex gap-1 lg:gap-2">
+        {links.map((link, index) => (
+          <motion.li key={index}>
+            <MotionLink
+              href={link.href}
+              className={cn(
+                'rounded-md px-4 py-2 font-medium text-foreground duration-200 hover:text-primary lg:text-lg',
+                pathname === link.href ? 'bg-primary/30' : '',
+              )}
+            >
+              <motion.span>{link.name}</motion.span>
+            </MotionLink>
+          </motion.li>
+        ))}
+      </ul>
     </nav>
   );
 }
